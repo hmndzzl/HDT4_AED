b2739d9ce07675595cd8fd2ad2110eedf5f802cb
Pxdro-410 <pedrojuliocaso@gmail.com>
2025-02-19 00:18:00 -0600

    Creacion de la clase StackArrayList

---
+++ mi-proyecto/StackArrayList.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+
+public class StackArrayList<E> implements Stack<E> {
+    private ArrayList<E> data;
+
+    /**
+     * Constructor 
+     */
+    public StackArrayList() {
+        data = new ArrayList<>();
+    }
+
+    /**
+     * añade un elemento al stack
+     * @param item es el elemento que se agregara
+     */
+    public void push(E item) {
+        data.add(item);
+    }
+
+    /**
+     * borra y devuelve el ultimo elemento añadido
+     * @return lo eliminado
+     * @throws IndexOutOfBoundsException si no hay elemento
+     */
+    public E pop() {
+        return data.remove(size() - 1);
+    }
+
+    /**
+     * Devuelve el ultimo elemento ingresado 
+     * @return El ultimo elemento ingresado
+     * @throws IndexOutOfBoundsException si esta vacío
+     */
+    public E peek() {
+        return data.get(size() - 1);
+    }
+
+    /**
+     * Devuelve el número de elementos 
+     * @return Cantidad de elementos en el stack
+     */
+    public int size() {
+        return data.size();
+    }
+
+    /**
+     * Verifica si el stack esta vacio
+     * @return false o true dependiendo si esta o no vacio el stack
+     */
+    public boolean empty() {
+        return data.isEmpty();
+    }
+}
