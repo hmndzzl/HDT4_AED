77766cbb58fcf27f854851ef739dab8abd314997
Pxdro-410 <pedrojuliocaso@gmail.com>
2025-02-19 00:00:02 -0600

    Clase Stack y StackVector

---
+++ mi-proyecto/Stack.java
@@ -0,0 +1,34 @@
+public interface Stack<E> {
+
+   /**
+    * Agrega un elemento a la pila o stack
+    * @param item Elemento a agregar.
+    */
+   public void push(E item);
+   
+   /**
+    * Elimina y devuelve el último elemento
+    * @return Elemento eliminado.
+    * @throws IndexOutOfBoundsException si la pila está vacía.
+    */
+   public E pop();
+   
+   /**
+    * Devuelve el últomo elemento ingresado en el stack.
+    * @return el último elemento.
+    * @throws IndexOutOfBoundsException si la pila está vacía.
+    */
+   public E peek();
+   
+   /**
+    * Verifica si la pila está vacía.
+    * @return true si la pila está vacía, @return false en caso contrario.
+    */
+   public boolean empty();
+   
+   /**
+    * Devuelve el número de elementos en el stack o pila.
+    * @return Cantidad de elementos en stack.
+    */
+   public int size();
+}
